240d6b7205a456a1afabd942a2a770ce6e7214b5
Kevin Xia <kevinmxia@gmail.com>
2019-06-22 23:09:36 -0400

    Convert images to more usable form.

--- TemporalDifferenceLearning/overcooked/screen_environment.py
+++ TemporalDifferenceLearning/overcooked/screen_environment.py
@@ -11,5 +11,18 @@ def screen_record():
             cv2.destroyAllWindows()
             break
 
+
+def process_img(image):
+    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    return processed_img
+
+
 if __name__ == "__main__":
-    screen_record()
+    while True:
+        screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+        new_screen = process_img(screen)
+        cv2.imshow('window', new_screen)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
